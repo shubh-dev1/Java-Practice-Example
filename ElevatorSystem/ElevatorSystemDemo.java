fc774c4b871ee72ad43a9e4f2171aa30ae77e47a
Shubham <Singhalshubham610@gmail.com>
2025-05-06 11:08:31 +0530

    Elevator System

---
+++ ElevatorSystem/ElevatorSystemDemo.java
@@ -0,0 +1,15 @@
+package ElevatorSystem;
+
+public class ElevatorSystemDemo {
+  public static void main(String[] args) {
+    run();
+  }
+  public static void run(){
+    ElevatorController controller = new ElevatorController(3,5);
+    controller.requestElevator(5,10);
+    controller.requestElevator(3,7);
+    controller.requestElevator(8,2);
+    controller.requestElevator(1,9);
+
+  }
+}
